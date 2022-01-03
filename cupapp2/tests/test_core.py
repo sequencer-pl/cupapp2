8a0d9344193796bb9c4a3cc8814951c20e943490
sequencer <sequencer@o2.pl>
2022-01-03 21:29:04 +0100

    Move tests to seperate files in tests folder

--- cupapp2/tests/test_core.py
+++ cupapp2/tests/test_core.py
@@ -1,23 +1,7 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
 from parameterized import parameterized
 
-from cupapp.core import League, WrongNumberOfPlayersException, SmartLeague  # SurvivalCup
-from cupapp.models import Tournament
-
-
-class AdminTest(TestCase):
-    cupName = 'CupOne'
-    cupDesc = 'SomeDesc'
-
-    def setUp(self):
-        User.objects.create_superuser(username='testuser', email='test@test.com', password='pass')
-        Tournament.objects.create(name=self.cupName, description=self.cupDesc, form=('cup', 'cup'),
-                                  owner=User.objects.get(username='testuser'))
-
-    def test_if_proper_cup_name_in_admin(self):
-        cup_one = Tournament.objects.get(name=self.cupName)
-        self.assertEqual(str(cup_one), f'{self.cupName}')
+from cupapp.core import League, WrongNumberOfPlayersException, SmartLeague
 
 
 class TournamentLeagueTest(TestCase):
